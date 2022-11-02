9621c7364345308bc6bce8e2f9ff5136a49870c2
Егор <nelunull@gmail.com>
2022-11-02 09:49:38 +0300

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,31 @@
+<!-- eslint-disable vue/no-unused-components -->
+<!-- eslint-disable vue/no-deprecated-slot-attribute -->
 <template>
   <div class="hello">
+    <ion-button  fill="outline" id="click-trigger">Open</ion-button>
+    <ion-button  >NORMAL TEXT</ion-button>
+    <p>{{ message }}</p>
+    <small class="text-muted mb-2 mt-4"> GOOD GOOD</small>
+    <small class="text-muted mb-2 mt-4"> GOOD GOOD</small>
+    BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD
+ <ion-modal
+
+                        :initial-breakpoint="0.5"
+                        :breakpoints="[0, 0.25, 0.5]"
+                        trigger="click-trigger"
+                        trigger-action="click"
+                      >
+                        <ion-content class="ion-padding">
+                          <div class="text-center d-flex flex-column mt-3">
+                            <small class="text-muted mt-2">dfsdfssdfdsfdfs</small>
+                            <ion-button  fill="outline" id="click-trigger">fdggfdsfgdsgsdrfsdfgdfg</ion-button>
+                            <ion-button  >BAD TEXT</ion-button>
+                            <small class="text-muted mb-2 mt-4"> BAD BAD</small>
+                            BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD BAD 
+                          </div>
+                        </ion-content>
+                      </ion-modal>
+
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -31,8 +57,40 @@
 </template>
 
 <script>
+  import {
+    IonButtons,
+    IonButton,
+    IonModal,
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonItem,
+    IonInput,
+    IonLabel,
+  } from '@ionic/vue';
+
 export default {
   name: 'HelloWorld',
+  components: {
+      // eslint-disable-next-line vue/no-unused-components
+      IonButtons,
+      IonButton,
+      IonModal,
+      // eslint-disable-next-line vue/no-unused-components
+      IonHeader,
+      IonContent,
+      // eslint-disable-next-line vue/no-unused-components
+      IonToolbar,
+      // eslint-disable-next-line vue/no-unused-components
+      IonTitle,
+      // eslint-disable-next-line vue/no-unused-components
+      IonItem,
+      // eslint-disable-next-line vue/no-unused-components
+      IonInput,
+      // eslint-disable-next-line vue/no-unused-components
+      IonLabel,
+    },
   props: {
     msg: String
   }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+/* ionic */
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/core.css';
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css'; /* Core CSS required for Ionic components to work properly */
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
 
-createApp(App).mount('#app')
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+
+import { IonicVue } from '@ionic/vue';
+
+
+createApp(App).use(IonicVue, {
+  /* animated: !Capacitor.isNativePlatform(), */
+}).mount('#app')
